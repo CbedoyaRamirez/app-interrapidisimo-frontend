6e922706bd0872e9b0585cb46dabdacb423635a4
Carlos Bedoya <bedoyacarlosalberto@gmail.com>
2024-12-04 10:10:50 -0500

    pba de slack

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,10 +18,8 @@ import { ModalUpdateMateriaComponent } from './modal-update-materia/modal-update
 })
 export class HomeComponent implements OnInit {
 
-  
 
 
-  
   listMaterias: Materia[];
   listMateriasProfesores: Profesor[];
   listProfesores: Profesor[];
